f35f8ba3e6d717426094042c28787e466dd8fbe6
Reshma <karkireshma707@gmail.com>
2024-02-24 02:44:10 +0545

    Initial commit

---
+++ Car/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Home/Hero/Hero'
+import Featured from '../components/Home/Featured/Featured'
+
+const Home = () => {
+    return (
+        <>
+        <Navbar/>
+        <Hero/>
+        <Featured/>
+        </>
+    )
+}
+
+export default Home;
