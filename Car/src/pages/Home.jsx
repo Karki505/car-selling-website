27f55d226328ee9afd08e9a3895576288be4314d
Reshma <karkireshma707@gmail.com>
2024-02-26 00:19:37 +0545

    Footer

--- Car/src/pages/Home.jsx
+++ Car/src/pages/Home.jsx
@@ -3,6 +3,9 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Home/Hero/Hero'
 import Featured from '../components/Home/Featured/Featured'
 import WhyUs from '../components/Home/WhyUs/WhyUs'
+import OurMission from '../components/Home/Mission/OurMission'
+import CarNews from '../components/Home/News/CarNews'
+import Footer from '../components/Footer'
 
 const Home = () => {
     return (
@@ -11,6 +14,9 @@ const Home = () => {
         <Hero/>
         <Featured/>
         <WhyUs/>
+        <OurMission/>
+        <CarNews/>
+        <Footer/>
         </>
     )
 }
